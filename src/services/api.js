1a5ab783f9857aa42274ebec3f6430d4dd61648f
Joni Mustaniemi <61048202+JoniMustaniemi@users.noreply.github.com>
2024-09-08 00:49:47 +0300

    Code cleaning

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/', // Change this to your deployed backend URL
+  baseURL: 'http://localhost:3000/',
   withCredentials: false,
   headers: {
     'Accept': 'application/json',
